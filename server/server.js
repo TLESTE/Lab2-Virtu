bb59cc83e294d0fd237b58c0b4fe45d0e9656587
NatisRevolis <madore.leste@gmail.com>
2025-12-13 18:08:08 +0100

    changes

--- server/server.js
+++ server/server.js
@@ -1,34 +1,32 @@
-const express=require ("express");
-const cors=require("cors");
-const {Pool}= require("pg");
+const express = require('express')
+const cors = require('cors')
+const { Pool } = require('pg')
 
-const app=express();
-app.use(cors());
-app.use(express.json());
+const app = express()
+app.use(cors())
+app.use(express.json())
 
 const pool = new Pool({
-    user:process.env.PGUSER,
-    host: process.env.PGHOST,
-    database: process.env.PGDATABASE,
-    password: process.env.PGPASSWORD,
-    port: process.env.PGPORT,
-});
+  user: process.env.PGUSER,
+  host: process.env.PGHOST,
+  database: process.env.PGDATABASE,
+  password: process.env.PGPASSWORD,
+  port: process.env.PGPORT
+})
 
-app.get("/all",async(req, res)=>{
-    const result=await pool.query("SELECT * FROM texte");
-    res.json(result.rows);
-});
-app.post("/all",async(req,res)=>{
-    try{
-        console.log(req.body);
-        const {text}=req.body;
-        await pool.query("INSERT INTO texte (content) VALUES ($1)",[text]);
-        res.json({status:"ok"});
-    }catch (err){
-        console.error("Raté",err);
-        res.status(500).json({error: "Erreur serveur"});
-    }
-    
-
-});
-app.listen(3001,()=>{console.log("API OK")});
+app.get('/all', async (req, res) => {
+  const result = await pool.query('SELECT * FROM texte')
+  res.json(result.rows)
+})
+app.post('/all', async (req, res) => {
+  try {
+    console.log(req.body)
+    const { text } = req.body
+    await pool.query('INSERT INTO texte (content) VALUES ($1)', [text])
+    res.json({ status: 'ok' })
+  } catch (err) {
+    console.error('Raté', err)
+    res.status(500).json({ error: 'Erreur serveur' })
+  }
+})
+app.listen(3001, () => { console.log('API OK') })
